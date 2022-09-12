5e620d8f9d81325e9c853b482824f3880b80dbde
Sarah López <a3521110542@alumno.uttehuacan.edu.mx>
2022-09-11 20:44:30 -0500

    Vue3 contador 500A

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+    export default{
+        data: () => ({
+            contador: 500,
+        }),
+        methods:{
+            incrementar(){
+                this.contador++;
+            },
+            disminuir(){
+                this.contador--;
+            },
+            resetear(){
+                this.contador=500;
+            },
+        }
+    };
+</script>
+
+<template>
+    <img src="./assets/logo.svg" alt="Logo VUE" width="125" height="125">
+    <div class="container">
+        <br>
+        <h1>Hola desde VUE.js</h1>
+        <hr>
+        <h2>Contador {{ contador}}</h2>
+        <button @click="incrementar()">+1</button>
+        <button @click="disminuir">-1</button>
+        <button @click="resetear">Reset</button>
+    </div>
+</template>
+
+<style scoped>
+    .container{
+        text-align: center;
+    }
+    small{
+        color: #666;
+    }
+    h1{
+        font-weight: bolder;
+    }
+    h2{
+        font-size: 2.5rem;
+    }
+    button{
+        cursor: pointer;
+        font-size: 1.5rem;
+        margin: 0px 8px;
+    }
+</style>
